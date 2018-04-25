b676f784f3fdd6c754e98d0c2a81345a928b59df
Vlad <guf_of_vlad@mail.ru>
2018-04-25 18:56:55 +0300

    addStatusColoring

--- src/main/resources/static/js/ordersRealization.js
+++ src/main/resources/static/js/ordersRealization.js
@@ -4,7 +4,23 @@
       lengthChange: false,
       oLanguage: {
         sSearch: "Поиск:"
-      }
+      },
+      columnDefs: [
+        { targets: '_all', className: "cells_border " }
+      ],
+      "createdRow": function( row, data, dataIndex){
+        // if( data[2] ==  `someVal`){
+        //   $(row).addClass('redClass');
+        // }
+        switch($(row).data('state')) {
+          case 'Awaiting' : $(row).addClass('awaiting'); break;
+          case 'Transported' : $(row).addClass('transported'); break;
+          case 'Arrived' : $(row).addClass('arrived'); break;
+          default: console.log('YOOOOO'); break;
+        }
+
+      },
+
     });
 
 })();
