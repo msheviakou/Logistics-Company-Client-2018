14c8b9c355b43ea36e850a0d28242d7fd35c5a37
Veluygin <egorvel123@gmail.com>
2018-04-30 19:51:49 +0300

    wtf

--- src/main/java/edu/bsuir/controller/user/UserDelete.java
+++ src/main/java/edu/bsuir/controller/user/UserDelete.java
@@ -2,12 +2,11 @@ package edu.bsuir.controller.user;
 
 import edu.bsuir.model.Users;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
@@ -23,11 +22,12 @@ public class UserDelete {
 
         restTemplate.delete(URL_USER + "/" + userId);
 
-        ResponseEntity<Users> responseEntity = restTemplate.getForEntity(URL_USER + "/" + userId, Users.class);
-        if (responseEntity.getStatusCode() == HttpStatus.OK) {
-            System.err.println("xyu");
+        try {
+            restTemplate.getForEntity(URL_USER + "/" + userId, Users.class);
+        }catch (HttpServerErrorException ignored){
             return "redirect:/administration";
         }
+        System.err.println("xyu");
         return "redirect:/administration";
     }
 }
