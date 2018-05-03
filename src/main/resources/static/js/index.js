f4adddaa20b5eeaf15b28d8b5341c9fb47e5fce6
Maksim Shevyakov <shevyakovmmbsuir@gmail.com>
2018-05-03 23:53:32 +0300

    03.05.2018 makdev

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,93 +1,146 @@
 ;(() => {
-  $( document ).ready(function() {
-    showViewPage('ordersRealization');
-    changeNavigationText('Транспортные заказы');
-    changeBreadCrumbText('Заказы в реализации');
-  });
-
-  const showViewPage = (name) => {
-    $.ajax({
-      url: name,
-      cache: false,
-      success: function(html) {
-        let $doc = $('<div></div>').html(html);
-        let $el = $doc.find(`.${name}_container`);
-        $('.content').append($el);
-      },
+    $(document).ready(function () {
+        showViewPage('ordersRealization');
+        changeNavigationText('Транспортные заказы');
+        changeBreadCrumbText('Заказы в реализации');
     });
-  };
 
-  const changeNavigationText = (text) => {
-    $('.navigation_panel__text').text(text);
-  };
+    const showViewPage = (name) => {
+        $.ajax({
+            url: name,
+            cache: false,
+            success: function (html) {
+                let $doc = $('<div></div>').html(html);
+                let $el = $doc.find(`.${name}_container`);
+                $('.content').append($el);
+            },
+        });
+    };
 
-  const changeBreadCrumbText = (text) => {
-    $('.current_place_on_site').text(text);
-  };
+    const changeNavigationText = (text) => {
+        $('.navigation_panel__text').text(text);
+    };
 
-  const clearDynamicalField = () => {
-    $('.content').html('');
-  };
+    const changeBreadCrumbText = (text) => {
+        $('.current_place_on_site').text(text);
+    };
 
-  // -------------EVENTS-------------------
-  $('.site_menu__list').click((event) => {
-    let { target } = event;
+    const clearDynamicalField = () => {
+        $('.content').html('');
+    };
 
-    while (target != this) {
-      if (target.classList.contains('site_menu_item')) {
-        clearDynamicalField();
-        showViewPage(target.dataset.action);
+    // -------------EVENTS-------------------
+    $('.site_menu__list').click((event) => {
+        let {target} = event;
 
-        switch (target.dataset.action) {
-          case 'ordersRealization':
-            changeNavigationText('Транспортные заказы');
-            changeBreadCrumbText('Заказы в реализации');
-            break;
-          case 'addOrder':
-            changeBreadCrumbText('Добавить заказ');
-            changeNavigationText('Заказ на загрузку');
-            break;
-          default: return;
+        while (target != this) {
+            if (target.classList.contains('site_menu_item')) {
+                clearDynamicalField();
+                showViewPage(target.dataset.action);
+
+                switch (target.dataset.action) {
+                    case 'ordersRealization':
+                        changeNavigationText('Транспортные заказы');
+                        changeBreadCrumbText('Заказы в реализации');
+                        break;
+                    case 'addOrder':
+                        changeBreadCrumbText('Добавить заказ');
+                        changeNavigationText('Заказ на загрузку');
+                        break;
+                    default:
+                        return;
+                }
+                return;
+            }
+            target = target.parentNode;
         }
-        return;
-      }
-      target= target.parentNode;
-    }
 
-  });
+    });
 
-  $('.site_menu_small').click((event) => {
-    let { target } = event;
+    $('.site_menu_small').click((event) => {
+        let {target} = event;
 
-    while (target != this) {
-      if (target.classList.contains('menu_item_small')) {
-        clearDynamicalField();
-        showViewPage(target.dataset.action);
+        while (target != this) {
+            if (target.classList.contains('menu_item_small')) {
+                clearDynamicalField();
+                showViewPage(target.dataset.action);
 
-        switch (target.dataset.action) {
-          case 'statistics':
+                switch (target.dataset.action) {
+                    case 'statistics':
+                        $.ajax({
+                            url: 'http://localhost:8080/orders',
+                            type: 'GET',
+                            dataType: 'json',
+                            crossDomain: true,
+                            crossOrigin: true,
+                            success: function (listOfOrders) {
+                                let counter = 0; // Количество заказов за месяц
 
-            break;
-          case 'trucks':
+                                var monthMap = new Map();
+                                monthMap.set("01", "Январь");
+                                monthMap.set("02", "Февраль");
+                                monthMap.set("03", "Март");
+                                monthMap.set("04", "Апрель");
+                                monthMap.set("05", "Май");
+                                monthMap.set("06", "Июнь");
+                                monthMap.set("07", "Июль");
+                                monthMap.set("08", "Август");
+                                monthMap.set("09", "Сентябрь");
+                                monthMap.set("10", "Октябрь");
+                                monthMap.set("11", "Ноябрь");
+                                monthMap.set("12", "Декабрь");
 
-            break;
-          case 'administration':
-            changeBreadCrumbText('Администрирование');
-            changeNavigationText('Управление пользователями');
-            break;
-          case 'settings':
+                                let orderMap = new Map(); // Ключ: количество заказов, значение: месяц (цифры)
 
-            break;
-          default: return;
+                                listOfOrders.forEach(function (element) {
+                                    let monthOfOrder = element.dateOfOrder.split('-')[1];
+
+                                    if (orderMap.get(monthOfOrder) !== 0)
+                                        counter = counter + 1;
+                                    else counter = 0;
+                                    orderMap.set(monthOfOrder, counter);
+                                })
+
+                                monthMap.forEach(function (key, value) {
+
+                                })
+
+                                /* Start Graphic */
+                                var data = {
+                                    label: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+                                    series: [
+                                        [1, 5, 7, 4, 3, 6, 7, 4, 9, 2, 11, 12]
+                                    ]
+                                };
+
+                                var options = {
+                                    width: '1100px',
+                                    height: '500px'
+                                }
+
+                                new Chartist.Line('.ct-chart', data, options);
+                                /* End Graphic */
+                            },
+                        });
+                        break;
+                    case 'trucks':
+
+                        break;
+                    case 'administration':
+                        changeBreadCrumbText('Администрирование');
+                        changeNavigationText('Управление пользователями');
+                        break;
+                    case 'settings':
+
+                        break;
+                    default:
+                        return;
+                }
+                return;
+            }
+            target = target.parentNode;
         }
-        return;
-      }
-      target= target.parentNode;
-    }
-  });
-
-
-
+    });
 
 
 })();
